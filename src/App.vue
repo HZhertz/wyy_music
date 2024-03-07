8f48576613864d2aa1c6541dc9104307b75d5099
XuboZheng <HZhertz76@gmail.com>
2024-03-07 17:07:14 +0800

    feat(index): 新增首页sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <router-view></router-view>
+  <el-container>
+    <el-aside><Sidebar /></el-aside>
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+  </el-container>
 </template>
 
-<script>
-export default {
-  setup() {
-    return {}
-  }
-}
+<script setup>
+import Sidebar from '@/components/Sidebar.vue'
 </script>
 
 <style></style>
