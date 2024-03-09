36aca31f9cd12b0265c02beca33749f827ecd813
XuboZheng <HZhertz76@gmail.com>
2024-03-09 21:13:58 +0800

    feat(artist): 新增歌手页

--- src/apis/http.js
+++ src/apis/http.js
@@ -92,6 +92,22 @@ const mv = ({ area = '', type = '', order = '', limit = 50, offset = 0 }) => {
   )
 }
 
+/* ********* 歌手 ********* */
+// 获取歌手列表
+/*
+ * type: -1:全部; 1:男歌手; 2:女歌手; 3:乐队
+ * area: -1:全部; 7华语; 96欧美; 8:日本; 16韩国; 0:其他
+ * initial: 按首字母索引查找参数, 热门传-1, #传0
+ * limit: 30
+ * offset: 0
+ */
+const artistList = ({ type = -1, area = -1, initial = '', limit = 50, offset = 0 }) => {
+  return api.get(
+    `/artist/list?type=${type}&area=${area}&initial=${initial}&limit=${limit}&offset=${offset}`,
+    {}
+  )
+}
+
 export {
   login,
   logout,
@@ -109,5 +125,6 @@ export {
   topArtists,
   topListDetail,
   listDetail,
-  catlist
+  catlist,
+  artistList
 }
